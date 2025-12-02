9a496d5d789e20c8e65ef79d1b749484c4af70ed
crackededed <90209774+crackededed@users.noreply.github.com>
2025-12-02 17:04:39 +0200

    fix crash when switching streams
    (cherry picked from commit 995ae0ae820ad43b9a390fc2b9b270bec288df48)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.github.andreyasadchy.xtra"
         minSdk = 23
         targetSdk = 36
-        versionCode = 310
-        versionName = "2.51.5"
+        versionCode = 311
+        versionName = "2.51.6"
     }
 
     buildTypes {
