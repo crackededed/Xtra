4b7f742d8a499530da271768d844063a6ed2f866
crackededed <90209774+crackededed@users.noreply.github.com>
2025-12-17 18:21:45 +0200

    player ui fixes
    (cherry picked from commit 35d71d254332b58665de21627fe3fb54049f61f1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.github.andreyasadchy.xtra"
         minSdk = 23
         targetSdk = 36
-        versionCode = 312
-        versionName = "2.52.0"
+        versionCode = 313
+        versionName = "2.52.1"
     }
 
     buildTypes {
