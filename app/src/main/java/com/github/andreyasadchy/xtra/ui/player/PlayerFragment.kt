167b7f4555a0aaf86ab93e59b2cbb03564ac2091
mqus <8398165+mqus@users.noreply.github.com>
2025-12-17 17:10:27 +0100

    Add settings to handle pip background playback (#823)
    * Add settings to handle pip background playback
    
    * DO not show pip settings when PiP is not supported
    
    * changes
    
    ---------
    
    Co-authored-by: crackededed <90209774+crackededed@users.noreply.github.com>
    (cherry picked from commit 2675fc840389cb0edc0862508bfec58cbba8af8c)

--- app/src/main/java/com/github/andreyasadchy/xtra/ui/player/PlayerFragment.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/ui/player/PlayerFragment.kt
@@ -108,7 +108,7 @@ abstract class PlayerFragment : BaseNetworkFragment(), RadioButtonDialogFragment
 
     protected var videoType: String? = null
     private var isPortrait = false
-    private var isMaximized = true
+    var isMaximized = true
     private var isChatOpen = true
     private var isKeyboardShown = false
     private var resizeMode = 0
@@ -1871,7 +1871,12 @@ abstract class PlayerFragment : BaseNetworkFragment(), RadioButtonDialogFragment
 
     override fun onResume() {
         super.onResume()
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && requireActivity().isInPictureInPictureMode) {
+        val isInPIPMode = when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> requireActivity().isInPictureInPictureMode
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.O -> !useController && isMaximized
+            else -> false
+        }
+        if (isInPIPMode) {
             if (isPortrait) {
                 binding.chatLayout.gone()
             } else {
@@ -2046,7 +2051,12 @@ abstract class PlayerFragment : BaseNetworkFragment(), RadioButtonDialogFragment
             } else {
                 disableBackground()
             }
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O || !requireActivity().isInPictureInPictureMode) {
+            val isInPIPMode = when {
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> requireActivity().isInPictureInPictureMode
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.O -> !useController && isMaximized
+                else -> false
+            }
+            if (!isInPIPMode) {
                 chatLayout.hideKeyboard()
                 chatLayout.clearFocus()
                 initLayout()
