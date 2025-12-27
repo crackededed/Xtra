7b9216efc8686ef25fc047e974a2b09b51b10935
crackededed <90209774+crackededed@users.noreply.github.com>
2025-12-27 23:00:13 +0200

    default quality option for cellular networks
    (cherry picked from commit 3bbaabec47bd647dcd3d6a6f60b947d0f7094553)

--- app/src/main/java/com/github/andreyasadchy/xtra/ui/player/ExoPlayerFragment.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/ui/player/ExoPlayerFragment.kt
@@ -6,6 +6,8 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Build
 import android.os.IBinder
 import android.os.PowerManager
@@ -911,7 +913,10 @@ class ExoPlayerFragment : PlayerFragment() {
                             }
                         }
                     }
-                    if (prefs.getString(C.PLAYER_DEFAULTQUALITY, "saved") == "saved") {
+                    val connectivityManager = requireContext().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+                    val networkCapabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+                    val cellular = networkCapabilities?.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) == true
+                    if ((!cellular && prefs.getString(C.PLAYER_DEFAULTQUALITY, "saved") == "saved") || (cellular && prefs.getString(C.PLAYER_DEFAULT_CELLULAR_QUALITY, "saved") == "saved")) {
                         prefs.edit { putString(C.PLAYER_QUALITY, quality.key) }
                     }
                 }
