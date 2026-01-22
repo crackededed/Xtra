56162c6a237ba2fb54e722009c707eb1e2b0a4fc
crackededed <90209774+crackededed@users.noreply.github.com>
2026-01-22 23:37:22 +0200

    fix crash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin)
     alias(libs.plugins.kotlin.parcelize)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.ksp)
