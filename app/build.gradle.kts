662dab9b2245d4f7f6f2826d292abfc7951b0be9
crackededed <90209774+crackededed@users.noreply.github.com>
2026-02-21 19:56:51 +0200

    fix aspect ratio for portrait streams
    (cherry picked from commit cc576eeb1b415cd168c3db53837ee2eac4ff3e38)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.github.andreyasadchy.xtra"
         minSdk = 23
         targetSdk = 36
-        versionCode = 324
-        versionName = "2.54.0"
+        versionCode = 325
+        versionName = "2.54.1"
     }
 
     buildTypes {
