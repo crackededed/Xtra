b61dd46bce011a93b7c04e72e01e9250cf315759
crackededed <90209774+crackededed@users.noreply.github.com>
2026-05-20 20:08:36 +0300

    target android 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,14 +21,14 @@ android {
         }
     }
     namespace = "com.github.andreyasadchy.xtra"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.github.andreyasadchy.xtra"
         minSdk = 23
-        targetSdk = 36
-        versionCode = 334
-        versionName = "2.55.4"
+        targetSdk = 37
+        versionCode = 335
+        versionName = "2.56.0"
     }
 
     buildTypes {
