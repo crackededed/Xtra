49caf9a129ac24ab99a2810cac88d26343b455ce
crackededed <90209774+crackededed@users.noreply.github.com>
2026-05-20 20:05:18 +0300

    fix video offset on new playback service

--- app/src/main/java/com/github/andreyasadchy/xtra/ui/main/MainActivity.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/ui/main/MainActivity.kt
@@ -509,11 +509,6 @@ class MainActivity : AppCompatActivity() {
                                 closePlayer()
                             }
                             startVideo(video, offset, offset != null)
-                            if (prefs.getBoolean(C.PLAYER_USE_VIDEO_POSITIONS, true)) {
-                                video.id.toLongOrNull()?.let { id ->
-                                    viewModel.savePosition(id, offset ?: 0)
-                                }
-                            }
                         }
                         viewModel.video.value = null
                     }
@@ -976,6 +971,11 @@ class MainActivity : AppCompatActivity() {
             videoAnimatedPreviewURL = video.animatedPreviewURL,
             position = offset,
         ))
+        if (ignoreSavedPosition && prefs.getBoolean(C.PLAYER_USE_VIDEO_POSITIONS, true)) {
+            video.id?.toLongOrNull()?.let { id ->
+                viewModel.saveVideoPosition(id, offset ?: 0)
+            }
+        }
         val fragment = when (prefs.getString(C.PLAYER, C.EXOPLAYER)) {
             C.MEDIA_PLAYER -> MediaPlayerFragment()
             else -> ExoPlayerFragment()
