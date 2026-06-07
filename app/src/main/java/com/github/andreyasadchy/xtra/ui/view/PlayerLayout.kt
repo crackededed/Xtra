45d34d725a1ba0a0399fb15cd24fa5eeac04eb10
intsven <svenkunze99@gmail.com>
2026-06-07 23:30:52 +0200

    feat: implement pinch-to-zoom on video player

--- app/src/main/java/com/github/andreyasadchy/xtra/ui/view/PlayerLayout.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/ui/view/PlayerLayout.kt
@@ -2,7 +2,10 @@ package com.github.andreyasadchy.xtra.ui.view
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.ScaleGestureDetector
 import android.widget.FrameLayout
+import com.github.andreyasadchy.xtra.R
 
 class PlayerLayout : FrameLayout {
 
@@ -11,6 +14,16 @@ class PlayerLayout : FrameLayout {
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
     var isPortrait = false
+    private var scaleFactor = 1f
+    private val scaleGestureDetector = ScaleGestureDetector(context, object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+        override fun onScale(detector: ScaleGestureDetector): Boolean {
+            scaleFactor *= detector.scaleFactor
+            scaleFactor = scaleFactor.coerceIn(1f, 3f)
+            findViewById<FrameLayout>(R.id.aspectRatioFrameLayout)?.scaleX = scaleFactor
+            findViewById<FrameLayout>(R.id.aspectRatioFrameLayout)?.scaleY = scaleFactor
+            return true
+        }
+    })
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
@@ -21,5 +34,15 @@ class PlayerLayout : FrameLayout {
                 MeasureSpec.makeMeasureSpec(playerHeight, MeasureSpec.EXACTLY)
             )
         }
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        scaleGestureDetector.onTouchEvent(event)
+        return true
+    }
+
+    override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
+        scaleGestureDetector.onTouchEvent(ev)
+        return super.onInterceptTouchEvent(ev)
     }
 }
