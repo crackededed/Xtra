455b13d949c5dd6f7c61d1fb72a32cd98580f39a
crackededed <90209774+crackededed@users.noreply.github.com>
2026-08-17 22:50:12 +0300

    fix crash

--- app/src/main/java/com/github/andreyasadchy/xtra/ui/settings/CustomProxySettingsAdapter.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/ui/settings/CustomProxySettingsAdapter.kt
@@ -2,6 +2,7 @@ package com.github.andreyasadchy.xtra.ui.settings
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
@@ -55,17 +56,32 @@ class CustomProxySettingsAdapter(
                 if (online != null) {
                     if (online) {
                         status.text = context.getString(R.string.online)
-                        status.setTextColor(context.resources.getColor(R.color.online, context.theme))
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            status.setTextColor(context.resources.getColor(R.color.online, context.theme))
+                        } else {
+                            @Suppress("DEPRECATION")
+                            status.setTextColor(context.resources.getColor(R.color.online))
+                        }
                     } else {
                         status.text = context.getString(R.string.offline)
-                        status.setTextColor(context.resources.getColor(R.color.offline, context.theme))
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            status.setTextColor(context.resources.getColor(R.color.offline, context.theme))
+                        } else {
+                            @Suppress("DEPRECATION")
+                            status.setTextColor(context.resources.getColor(R.color.offline))
+                        }
                     }
                 } else {
                     status.text = context.getString(R.string.loading)
                     val id = context.obtainStyledAttributes(intArrayOf(android.R.attr.textColorSecondary)).use {
                         it.getResourceId(0, 0)
                     }
-                    status.setTextColor(context.resources.getColor(id, context.theme))
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        status.setTextColor(context.resources.getColor(id, context.theme))
+                    } else {
+                        @Suppress("DEPRECATION")
+                        status.setTextColor(context.resources.getColor(id))
+                    }
                 }
             } else {
                 status.visibility = View.GONE
