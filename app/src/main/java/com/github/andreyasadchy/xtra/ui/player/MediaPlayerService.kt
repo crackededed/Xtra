ce14e50b40a104688d1044f2409e9713bb7cdfe9
crackededed <90209774+crackededed@users.noreply.github.com>
2026-09-17 16:43:30 +0300

    add codec params to proxy url
    (cherry picked from commit 34e67314452e0c7c4241c780191261c98dfc004d)

--- app/src/main/java/com/github/andreyasadchy/xtra/ui/player/MediaPlayerService.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/ui/player/MediaPlayerService.kt
@@ -538,7 +538,10 @@ class MediaPlayerService : BasePlaybackService() {
                                     val source = uri.getQueryParameter("allow_source") == null
                                     val audio = uri.getQueryParameter("allow_audio_only") == null
                                     val lowLatency = uri.getQueryParameter("fast_bread") == null
-                                    if (source || audio || lowLatency) {
+                                    val unavailable = uri.getQueryParameter("include_unavailable") == null
+                                    val supportedCodecs = prefs().getString(C.TOKEN_SUPPORTED_CODECS, "av1,h265,h264")
+                                    val codecs = !supportedCodecs.isNullOrBlank() && uri.getQueryParameter("supported_codecs") == null
+                                    if (source || audio || lowLatency || unavailable || codecs) {
                                         uri.buildUpon().apply {
                                             if (source) {
                                                 appendQueryParameter("allow_source", "true")
@@ -548,6 +551,12 @@ class MediaPlayerService : BasePlaybackService() {
                                             }
                                             if (lowLatency) {
                                                 appendQueryParameter("fast_bread", "true")
+                                            }
+                                            if (unavailable) {
+                                                appendQueryParameter("include_unavailable", "true")
+                                            }
+                                            if (codecs) {
+                                                appendQueryParameter("supported_codecs", supportedCodecs)
                                             }
                                         }.build()
                                     } else uri
