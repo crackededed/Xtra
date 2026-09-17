34e67314452e0c7c4241c780191261c98dfc004d
crackededed <90209774+crackededed@users.noreply.github.com>
2026-09-17 16:43:30 +0300

    add codec params to proxy url

--- app/src/main/java/com/github/andreyasadchy/xtra/ui/download/StreamDownloadService.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/ui/download/StreamDownloadService.kt
@@ -285,7 +285,9 @@ class StreamDownloadService : LifecycleService() {
                                     val source = uri.getQueryParameter("allow_source") == null
                                     val audio = uri.getQueryParameter("allow_audio_only") == null
                                     val lowLatency = uri.getQueryParameter("fast_bread") == null
-                                    if (source || audio || lowLatency) {
+                                    val unavailable = uri.getQueryParameter("include_unavailable") == null
+                                    val codecs = !supportedCodecs.isNullOrBlank() && uri.getQueryParameter("supported_codecs") == null
+                                    if (source || audio || lowLatency || unavailable || codecs) {
                                         uri.buildUpon().apply {
                                             if (source) {
                                                 appendQueryParameter("allow_source", "true")
@@ -295,6 +297,12 @@ class StreamDownloadService : LifecycleService() {
                                             }
                                             if (lowLatency) {
                                                 appendQueryParameter("fast_bread", "true")
+                                            }
+                                            if (unavailable) {
+                                                appendQueryParameter("include_unavailable", "true")
+                                            }
+                                            if (codecs) {
+                                                appendQueryParameter("supported_codecs", supportedCodecs)
                                             }
                                         }.build()
                                     } else uri
@@ -319,7 +327,9 @@ class StreamDownloadService : LifecycleService() {
                                             val source = uri.getQueryParameter("allow_source") == null
                                             val audio = uri.getQueryParameter("allow_audio_only") == null
                                             val lowLatency = uri.getQueryParameter("fast_bread") == null
-                                            if (source || audio || lowLatency) {
+                                            val unavailable = uri.getQueryParameter("include_unavailable") == null
+                                            val codecs = !supportedCodecs.isNullOrBlank() && uri.getQueryParameter("supported_codecs") == null
+                                            if (source || audio || lowLatency || unavailable || codecs) {
                                                 uri.buildUpon().apply {
                                                     if (source) {
                                                         appendQueryParameter("allow_source", "true")
@@ -329,6 +339,12 @@ class StreamDownloadService : LifecycleService() {
                                                     }
                                                     if (lowLatency) {
                                                         appendQueryParameter("fast_bread", "true")
+                                                    }
+                                                    if (unavailable) {
+                                                        appendQueryParameter("include_unavailable", "true")
+                                                    }
+                                                    if (codecs) {
+                                                        appendQueryParameter("supported_codecs", supportedCodecs)
                                                     }
                                                 }.build()
                                             } else uri
