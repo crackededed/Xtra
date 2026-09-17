5d89ccc5270a521c060db93e0e9bf1fc0fb03d5d
crackededed <90209774+crackededed@users.noreply.github.com>
2026-09-17 16:43:19 +0300

    fix video showing when restoring with audio only quality

--- app/src/main/java/com/github/andreyasadchy/xtra/ui/player/ExoPlayerService.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/ui/player/ExoPlayerService.kt
@@ -765,7 +765,16 @@ class ExoPlayerService : BasePlaybackService() {
                 STREAM -> {
                     started = true
                     serviceListener?.started()
-                    if (qualities.isNullOrEmpty()) {
+                    if (!qualities.isNullOrEmpty()) {
+                        if (quality?.name == VideoQuality.AUDIO_ONLY_QUALITY) {
+                            serviceListener?.changePlayerMode()
+                            player?.let { player ->
+                                player.trackSelectionParameters = player.trackSelectionParameters.buildUpon().apply {
+                                    setTrackTypeDisabled(androidx.media3.common.C.TRACK_TYPE_VIDEO, true)
+                                }.build()
+                            }
+                        }
+                    } else {
                         useCustomProxy = prefs().getBoolean(C.PLAYER_USE_CUSTOM_PROXY, true)
                         if (!useCustomProxy) {
                             useStreamProxy = prefs().getBoolean(C.PLAYER_USE_STREAM_PROXY, false)
@@ -794,6 +803,14 @@ class ExoPlayerService : BasePlaybackService() {
                     started = true
                     serviceListener?.started()
                     if (videoId != null) {
+                        if (!qualities.isNullOrEmpty() && quality?.name == VideoQuality.AUDIO_ONLY_QUALITY) {
+                            serviceListener?.changePlayerMode()
+                            player?.let { player ->
+                                player.trackSelectionParameters = player.trackSelectionParameters.buildUpon().apply {
+                                    setTrackTypeDisabled(androidx.media3.common.C.TRACK_TYPE_VIDEO, true)
+                                }.build()
+                            }
+                        }
                         loadVideo(restorePauseState)
                         if (title == null) {
                             updateVideoInfo()
@@ -820,6 +837,11 @@ class ExoPlayerService : BasePlaybackService() {
                             val url = quality?.url
                             if (url != null) {
                                 player?.let { player ->
+                                    if (quality?.name == VideoQuality.AUDIO_ONLY_QUALITY) {
+                                        player.trackSelectionParameters = player.trackSelectionParameters.buildUpon().apply {
+                                            setTrackTypeDisabled(androidx.media3.common.C.TRACK_TYPE_VIDEO, true)
+                                        }.build()
+                                    }
                                     val networkLibrary = prefs().getString(C.NETWORK_LIBRARY, C.OKHTTP)
                                     player.setMediaSource(
                                         HlsMediaSource.Factory(
