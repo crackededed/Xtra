3918379a14a1a9106961a3c20ddde0f3e5f74e41
crackededed <90209774+crackededed@users.noreply.github.com>
2026-09-17 16:43:51 +0300

    fix bookmark database update

--- app/src/main/java/com/github/andreyasadchy/xtra/model/ui/Bookmark.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/model/ui/Bookmark.kt
@@ -6,21 +6,21 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "bookmarks")
 class Bookmark(
     val videoId: String? = null,
-    val userId: String? = null,
+    var userId: String? = null,
     var userLogin: String? = null,
     var userName: String? = null,
     var userType: String? = null,
     var userBroadcasterType: String? = null,
     var userLogo: String? = null,
-    val gameId: String? = null,
-    val gameSlug: String? = null,
-    val gameName: String? = null,
-    val title: String? = null,
-    val createdAt: String? = null,
-    val thumbnail: String? = null,
-    val type: String? = null,
-    val duration: String? = null,
-    val animatedPreviewURL: String? = null,
+    var gameId: String? = null,
+    var gameSlug: String? = null,
+    var gameName: String? = null,
+    var title: String? = null,
+    var createdAt: String? = null,
+    var thumbnail: String? = null,
+    var type: String? = null,
+    var duration: String? = null,
+    var animatedPreviewURL: String? = null,
 ) {
 
     @PrimaryKey(autoGenerate = true)
