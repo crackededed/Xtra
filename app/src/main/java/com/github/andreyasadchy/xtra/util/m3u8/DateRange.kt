cd53743f7b538dd5fdb40cfdf55fd3047bc96fb2
crackededed <90209774+crackededed@users.noreply.github.com>
2026-09-26 15:49:53 +0300

    http proxy fixes

--- app/src/main/java/com/github/andreyasadchy/xtra/util/m3u8/DateRange.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/util/m3u8/DateRange.kt
@@ -2,10 +2,10 @@ package com.github.andreyasadchy.xtra.util.m3u8
 
 data class DateRange(
     val id: String,
-    val rangeClass: String?,
+    val className: String?,
     val startDate: String,
     val endDate: String?,
     val duration: Float?,
     val plannedDuration: Float?,
-    val ad: Boolean
+    val clientAttributes: List<Pair<String, String>>
 )
